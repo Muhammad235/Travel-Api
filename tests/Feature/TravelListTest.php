be0e125e9720bc55748719d67aa59ec5dc652bcd
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-09-16 23:55:10 +0100

    [create] Travel Factory

--- tests/Feature/TravelListTest.php
+++ tests/Feature/TravelListTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TravelListTest extends TestCase
 {
@@ -12,9 +13,13 @@ class TravelListTest extends TestCase
 
     public function test_travels_list_returns_paginated_data_correctly(): void
     {
+        Travel::factory(16)->create(['is_public' => true]);
 
         $response = $this->get('/api/v1/travels');
 
         $response->assertStatus(200);
+
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonPath('meta.last_page', 2);
     }
 }
